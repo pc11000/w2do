519e669e33e9c33733e3c34317aae25b0ff1ce6d
Fahim <fahim.ur.rehman95@gmail.com>
2021-01-27 22:29:03 +0500

    ci/cd

--- code-deploy-remote.sh
+++ code-deploy-remote.sh
@@ -2,8 +2,8 @@
 #ssh into server
 #sshpass -p "${{ secrets.DEV_DEPLOY_PASS }}" ssh -t -t  -oStrictHostKeyChecking=no ${{ secrets.DEV_DEPLOY_USER }}@${{ secrets.DEV_DEPLOY_IP }} -p ${{ secrets.DEV_DEPLOY_PORT }} << EOF
 #cd into the code directory and pull the latest changes
-cd /home/fiverr/w2do/
-git pull
+#cd /home/fiverr/w2do/
+#git pull
 #re-build the changes and make up via docker-compose
 sudo docker-compose build
 sudo docker-compose up -d
